8b6e2201cf7c66c623d166735e360e19c23950a9
Grady Blackwell <hattielover0131@gmail.com>
2026-08-12 14:22:38 -0600

    Added these files
    Co-authored-by: Leif Barton <leifwilliambarton@gmail.com>

---
+++ mainmanager.py
@@ -0,0 +1,35 @@
+import sys
+
+from PySide6.QtWidgets import (
+    QFrame,
+    QHBoxLayout,
+    QLabel,
+    QRadioButton,
+    QVBoxLayout,
+)
+
+from import_utils import ImportManager
+
+
+class TopBar(QFrame):
+    def __init__(self):
+        super().__init__()
+        self.boxes = []
+        bar_layout = QHBoxLayout(self)
+        bar_layout.setContentsMargins(5,5,5,5)
+        bar_layout.setSpacing(5)
+        for i in range(5):
+            box = self.AudioBox(i)
+            self.boxes.append(box)
+            bar_layout.addWidget(box, stretch=1)
+    class AudioBox(QFrame):
+        def __init__(self,index:int):
+            super().__init__()
+            self.setStyleSheet("QFrame { border: 1px solid #999; }")
+            v_layout = QVBoxLayout(self)
+            v_layout.setContentsMargins(4,4,4,4)
+            v_layout.setSpacing(2)
+            self.filename = QLabel("No file")
+            self.playbackbutton = QRadioButton()
+            v_layout.addWidget(self.filename)
+            v_layout.addWidget(self.playbackbutton)
---
+++ tracklist.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+
+from PySide6.QtCore import Signal
+from PySide6.QtWidgets import QTreeWidget, QTreeWidgetItem, QVBoxLayout, QWidget
+
+from import_utils import ImportManager
+
+
+class TrackList(QWidget):
+    track_selected: Signal = Signal(Path)
+    
+    def __init__(self, import_manager: ImportManager):
+        super().__init__()
+
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+
+        self.list = QTreeWidget()
+        self.list.setRootIsDecorated(False)
+        self.list.setHeaderLabels(["Path", "Name"])
+        self.list.setColumnHidden(0, True)
+        self.import_manager = import_manager
+
+        self.list.itemSelectionChanged.connect(self.on_track_selected)
+
+        self.reload()
+        self.list.show()
+
+        layout.addWidget(self.list)
+
+    def reload(self):
+        self.list.clear()
+        self.add_paths(self.import_manager.imported_paths)
+
+    def add_paths(self, paths: list[Path]):
+        self.list.addTopLevelItems([QTreeWidgetItem([str(path), path.name]) for path in paths])
+
+    def on_track_selected(self):
+        item = self.list.currentItem()
+        path = Path(item.text(0))
+        if not path.exists() or not path.is_file():
+            self.import_manager.remove(path)
+            # item = self.list.rehe item when we use takeItem.
+            idx = self.list.indexOfTopLevelItem(item)
+            item = self.list.takeTopLevelItem(idx)
+            del item
+            return
+        self.track_selected.emit(path)
