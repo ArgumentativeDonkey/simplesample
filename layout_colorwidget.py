08c1aa49e1f3a20b54e69de579cadf927024e326
Grady Blackwell <hattielover0131@gmail.com>
2026-08-12 12:59:20 -0600

    hi i forgot wha i did

--- layout_colorwidget.py
+++ layout_colorwidget.py
@@ -6,7 +6,6 @@ class Color(QWidget):
     def __init__(self, color):
         super().__init__()
         self.setAutoFillBackground(True)
-
         palette = self.palette()
         palette.setColor(QPalette.ColorRole.Window, QColor(color))
         self.setPalette(palette)
